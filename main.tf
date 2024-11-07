20cc144db9911d6d83c26e3d1514c45569476866
Dustin Dortch <me@dustindortch.com>
2024-11-07 11:17:57 -0500

    Add storage account and container

--- main.tf
+++ main.tf
@@ -16,12 +16,24 @@ data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
 
-# Storage Account
-
 resource "random_string" "rs" {
   length  = 5
   upper   = false
   special = false
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = join("", [var.storage_account_name, random_string.rs.result])
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication_type
+}
+
+resource "azurerm_storage_container" "sc" {
+  name                  = "docs"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
 }
 
 # Function App
--- variables.tf
+++ variables.tf
@@ -2,3 +2,38 @@ variable "resource_group_name" {
   description = "The name of the resource group in which the resources will be created."
   type        = string
 }
+
+variable "storage_account_name" {
+  description = "The name of the storage account."
+  type        = string
+}
+
+locals {
+  valid_storage_account_tiers = ["Standard", "Premium"]
+}
+
+variable "storage_account_tier" {
+  default     = "Standard"
+  description = "The tier of the storage account."
+  type        = string
+
+  validation {
+    condition     = contains(local.valid_storage_account_tiers, var.storage_account_tier)
+    error_message = "The value of storage_account_tier must be either ${join(", ", local.valid_storage_account_tiers)}."
+  }
+}
+
+locals {
+  valid_storage_account_replication_types = ["LRS", "GRS", "RAGRS", "ZRS", "GZRS", "RAGZRS"]
+}
+
+variable "storage_account_replication_type" {
+  default     = "LRS"
+  description = "The replication type of the storage account."
+  type        = string
+
+  validation {
+    condition     = contains(local.valid_storage_account_replication_types, var.storage_account_replication_type)
+    error_message = "The value of storage_account_replication_type must be either ${join(", ", local.valid_storage_account_replication_types)}."
+  }
+}
